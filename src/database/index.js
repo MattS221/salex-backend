a3dfe8137a7ebfe5edd3cfbbc0c35417f956b544
Matheus Silva <89227914+MattSm221@users.noreply.github.com>
2021-11-22 20:46:18 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/noderest');
+mongoose.connect('mongodb+srv://deploy:deploy@saleex.aac8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
